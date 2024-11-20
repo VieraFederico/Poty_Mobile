7f46c041bf33e08e0df9e2bbfac255cda0480187
lulostupnik <lulostupnik2003@gmail.com>
2024-11-20 20:19:56 -0300

    charge screen reactive

--- app/src/main/java/hci/mobile/poty/screens/charge/ChargeScreen.kt
+++ app/src/main/java/hci/mobile/poty/screens/charge/ChargeScreen.kt
@@ -1,28 +1,14 @@
 package hci.mobile.poty.screens.charge
+
+import ChargeScreenState
 import ChargeScreenViewModel
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import hci.mobile.poty.R
 import hci.mobile.poty.ui.components.BottomNavBar
 import hci.mobile.poty.ui.theme.GreenDark
@@ -40,101 +27,87 @@ import hci.mobile.poty.ui.theme.White
 import hci.mobile.poty.ui.theme.titleSmallSemiBold
 import hci.mobile.poty.utils.ErrorMessage
 import hci.mobile.poty.utils.NumberFieldWithLabel
-import hci.mobile.poty.utils.ThickTextFieldWithLabel
+import hci.mobile.poty.utils.WindowSizeClass
+import hci.mobile.poty.utils.calculateWindowSizeClass
+import hci.mobile.poty.ui.components.ResponsiveNavBar
+import hci.mobile.poty.utils.isLandscape
+import hci.mobile.poty.utils.isTablet
 
-@Preview
 @Composable
-fun DepositScreenPreview(){
-    ChargeScreen()
-}
-@Composable
-fun ChargeScreen(viewModel: ChargeScreenViewModel = remember { ChargeScreenViewModel() }) {
+fun ChargeScreen(
+    viewModel: ChargeScreenViewModel = remember { ChargeScreenViewModel() },
+    onNavigateBack: () -> Unit = {},
+    mockWindowSizeClass: WindowSizeClass? = null
+) {
     val state by viewModel.state.collectAsState()
+    val windowSizeClass = mockWindowSizeClass ?: calculateWindowSizeClass()
+
+    val isLandscape = windowSizeClass.isLandscape()
+    val contentPadding = if (windowSizeClass.isTablet()) 32.dp else 16.dp
 
     PotyTheme(darkTheme = true, dynamicColor = false) {
-        Scaffold(
-            modifier = Modifier.fillMaxSize(),
-            containerColor = MaterialTheme.colorScheme.secondary,
-            bottomBar = { BottomNavBar(onNavigate = { }) }
-        ) { innerPadding ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding),
-                verticalArrangement = Arrangement.Top
-            ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f),
-
-                    ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.background_scaffolding),
-                        contentDescription = null,
+        ResponsiveNavBar(
+            onNavigate = { /* Handle navigation */ },
+            mockWindowSizeClass = mockWindowSizeClass
+        ) {
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                containerColor = MaterialTheme.colorScheme.secondary,
+            ) { innerPadding ->
+                if (isLandscape) {
+                    Row(
                         modifier = Modifier
-                            .fillMaxSize(),
-                        contentScale = ContentScale.Crop
-                    )
-
-                    IconButton(
-                        onClick = { /*Cuando enseñen navegacion xddd*/ },
+                            .fillMaxSize()
+                            .padding(innerPadding)
                     ) {
-                        Surface(
-                            shape = CircleShape,
-                            color = GreenDark,
-                            modifier = Modifier.size(35.dp)
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.KeyboardArrowLeft,
-                                contentDescription = "Go Back",
-                                tint = White
-                            )
-                        }
-                    }
-                    Card(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        colors = CardDefaults.cardColors(
-                            containerColor = Color.Transparent
-                        ),
-                    ) {
-                        Column(
+                        ChargeHeaderSection(
                             modifier = Modifier
-                                .padding(16.dp)
-                                .fillMaxSize(),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Center,
-                        ) {
-                            Text(
-                                text = "Cobrar Dinero",
-                                style = MaterialTheme.typography.titleSmallSemiBold,
-                                color = White
-                            )
-                        }
-                    }
-                }
-
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(3.5f),
-                    colors = CardDefaults.cardColors(
-                        containerColor = MaterialTheme.colorScheme.onBackground,
-                    ),
-                    shape = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp, bottomEnd = 0.dp, bottomStart = 0.dp),
-                    elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
-                ) {
-
-                    when (state.currentStep) {
-                        1 -> StepOne(
-                            amount = state.amount,
-                            UpdateAmount = {viewModel.onEvent(ChargeScreenEvent.UpdateAmount(it))},
-                            NextStep = {viewModel.onEvent(ChargeScreenEvent.NextStep)},
-                            errorMessage = state.errorMessage
+                                .fillMaxHeight()
+                                .weight(1f),
+                            onNavigateBack = onNavigateBack,
+                            contentPadding = contentPadding,
+                            windowSizeClass = windowSizeClass
                         )
-                        2 -> StepTwo(
 
+                        ChargeContentSection(
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .weight(1f),
+                            state = state,
+                            viewModel = viewModel,
+                            contentPadding = contentPadding,
+                            windowSizeClass = windowSizeClass
+                        )
+                    }
+                } else {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        val headerWeight = if (windowSizeClass == WindowSizeClass.MediumTablet) {
+                            1f
+                        } else {
+                            0.7f
+                        }
+
+                        ChargeHeaderSection(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(headerWeight),
+                            onNavigateBack = onNavigateBack,
+                            contentPadding = contentPadding,
+                            windowSizeClass = windowSizeClass
+                        )
+
+                        ChargeContentSection(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(3.5f),
+                            state = state,
+                            viewModel = viewModel,
+                            contentPadding = contentPadding,
+                            windowSizeClass = windowSizeClass
                         )
                     }
                 }
@@ -144,26 +117,128 @@ fun ChargeScreen(viewModel: ChargeScreenViewModel = remember { ChargeScreenViewM
 }
 
 @Composable
+fun ChargeHeaderSection(
+    modifier: Modifier,
+    onNavigateBack: () -> Unit,
+    contentPadding: Dp,
+    windowSizeClass: WindowSizeClass
+) {
+    Box(
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.background_scaffolding),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
+        IconButton(
+            onClick = { onNavigateBack() },
+            modifier = Modifier.padding(contentPadding)
+        ) {
+            Surface(
+                shape = CircleShape,
+                color = GreenDark,
+                modifier = Modifier.size(35.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowLeft,
+                    contentDescription = "Go Back",
+                    tint = White
+                )
+            }
+        }
+
+        Card(
+            modifier = Modifier
+                .fillMaxSize(),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.Transparent
+            ),
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(contentPadding)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Text(
+                    text = "Cobrar Dinero",
+                    style = if (windowSizeClass.isTablet()) {
+                        MaterialTheme.typography.titleLarge
+                    } else {
+                        MaterialTheme.typography.titleSmallSemiBold
+                    },
+                    color = White
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ChargeContentSection(
+    modifier: Modifier,
+    state: ChargeScreenState,
+    viewModel: ChargeScreenViewModel,
+    contentPadding: Dp,
+    windowSizeClass: WindowSizeClass
+) {
+    Card(
+        modifier = modifier,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.onBackground,
+        ),
+        shape = RoundedCornerShape(
+            topStart = if (!windowSizeClass.isLandscape()) 30.dp else 0.dp,
+            topEnd = 30.dp,
+            bottomEnd = if (windowSizeClass.isLandscape()) 0.dp else 0.dp,
+            bottomStart = if (windowSizeClass.isLandscape()) 30.dp else 0.dp
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
+    ) {
+        when (state.currentStep) {
+            1 -> StepOne(
+                amount = state.amount,
+                UpdateAmount = { viewModel.onEvent(ChargeScreenEvent.UpdateAmount(it)) },
+                NextStep = { viewModel.onEvent(ChargeScreenEvent.NextStep) },
+                errorMessage = state.errorMessage,
+                contentPadding = contentPadding,
+                windowSizeClass = windowSizeClass
+            )
+            2 -> StepTwo(
+                contentPadding = contentPadding,
+                windowSizeClass = windowSizeClass
+            )
+        }
+    }
+}
+
+@Composable
 fun StepOne(
     amount: String,
     UpdateAmount: (String) -> Unit,
     NextStep: () -> Unit,
-    errorMessage: String
+    errorMessage: String,
+    contentPadding: Dp,
+    windowSizeClass: WindowSizeClass
 ) {
     Column(
         modifier = Modifier
-            .padding(16.dp)
+            .padding(contentPadding)
             .fillMaxSize(),
         verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
         NumberFieldWithLabel(
             label = "Monto a cobrar",
             value = amount.toFloatOrNull() ?: 0f,
             onValueChange = { newValue -> UpdateAmount(newValue.toString()) }
         )
         Spacer(modifier = Modifier.height(16.dp))
-
 
         Button(
             onClick = { NextStep() },
@@ -177,16 +252,70 @@ fun StepOne(
             ErrorMessage(message = errorMessage)
         }
     }
+}
 
-
+@Composable
+fun StepTwo(
+    contentPadding: Dp,
+    windowSizeClass: WindowSizeClass
+){
+    Text(
+        text = "STEP 2",
+        style = MaterialTheme.typography.bodyLarge,
+        modifier = Modifier.padding(contentPadding)
+    )
 }
 
 
 
+@Preview(
+    name = "Medium Phone Portrait",
+    device = "spec:width=411dp,height=914dp",
+    showBackground = true
+)
 @Composable
-fun StepTwo(){
-    Text(
-        text = "STEP 2",
-        style = MaterialTheme.typography.bodyLarge,
+fun ChargeScreenMediumPhonePortraitPreview() {
+    ChargeScreen(
+        onNavigateBack = {},
+        mockWindowSizeClass = WindowSizeClass.MediumPhone
+    )
+}
+
+@Preview(
+    name = "Medium Phone Landscape",
+    device = "spec:width=914dp,height=411dp",
+    showBackground = true
+)
+@Composable
+fun ChargeScreenMediumPhoneLandscapePreview() {
+    ChargeScreen(
+        onNavigateBack = {},
+        mockWindowSizeClass = WindowSizeClass.MediumPhoneLandscape
+    )
+}
+
+@Preview(
+    name = "Medium Tablet Portrait",
+    device = "spec:width=800dp,height=1280dp",
+    showBackground = true
+)
+@Composable
+fun ChargeScreenMediumTabletPortraitPreview() {
+    ChargeScreen(
+        onNavigateBack = {},
+        mockWindowSizeClass = WindowSizeClass.MediumTablet
+    )
+}
+
+@Preview(
+    name = "Medium Tablet Landscape",
+    device = "spec:width=1280dp,height=800dp",
+    showBackground = true
+)
+@Composable
+fun ChargeScreenMediumTabletLandscapePreview() {
+    ChargeScreen(
+        onNavigateBack = {},
+        mockWindowSizeClass = WindowSizeClass.MediumTabletLandscape
     )
 }
