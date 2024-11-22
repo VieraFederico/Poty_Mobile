f6f1b2029a367332e2a496f5e2ba057c28e89edb
mrossisei <mrossiseifert@itba.edu.ar>
2024-11-22 16:18:19 -0300

    payWithLink -> generateLink

--- app/src/main/java/hci/mobile/poty/data/network/api/PaymentApiService.kt
+++ app/src/main/java/hci/mobile/poty/data/network/api/PaymentApiService.kt
@@ -6,6 +6,7 @@ import hci.mobile.poty.data.network.model.NetworkBalancePayment
 import hci.mobile.poty.data.network.model.NetworkCard
 import hci.mobile.poty.data.network.model.NetworkCardPayment
 import hci.mobile.poty.data.network.model.NetworkLinkPayment
+import hci.mobile.poty.data.network.model.NetworkNewPaymentLink
 import hci.mobile.poty.data.network.model.NetworkPayment
 import retrofit2.Response
 import retrofit2.http.Body
@@ -22,7 +23,7 @@ interface PaymentApiService {
    suspend fun payWithCard(@Body cardPayment : NetworkCardPayment): Response<Unit>
 
    @POST("payment")
-   suspend fun payWithLink(@Body linkPayment: NetworkLinkPayment): Response<Unit>
+   suspend fun generateLink(@Body linkPayment: NetworkNewPaymentLink): Response<Unit>
 
    @GET("payment")
    suspend fun getPayments(): Response<List<NetworkPayment>>
--- app/src/main/java/hci/mobile/poty/data/network/model/NetworkLinkPayment.kt
+++ app/src/main/java/hci/mobile/poty/data/network/model/NetworkLinkPayment.kt
@@ -1,7 +1,5 @@
 package hci.mobile.poty.data.network.model
 
 class NetworkLinkPayment(
-    val amount: Double,
-    val description: String,
-    val type: String = "LINK"
+
 )
