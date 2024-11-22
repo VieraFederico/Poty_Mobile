2760f2a9965b6ca570b55d23afd760fdb12499f0
mrossisei <mrossiseifert@itba.edu.ar>
2024-11-22 15:42:36 -0300

    getCards()

--- app/src/main/java/hci/mobile/poty/data/repository/PaymentRepository.kt
+++ app/src/main/java/hci/mobile/poty/data/repository/PaymentRepository.kt
@@ -14,12 +14,31 @@ import hci.mobile.poty.data.model.CardPayment
 import hci.mobile.poty.data.model.LinkPayment
 import hci.mobile.poty.data.network.model.NetworkBalancePayment
 import hci.mobile.poty.data.network.model.NetworkCardPayment
+import hci.mobile.poty.data.model.Payment
+import kotlinx.coroutines.sync.withLock
 
 class PaymentRepository (
     private val remoteDataSource: PaymentRemoteDataSource
 ){
+    // Mutex to make writes to cached values thread-safe
+    private val paymentsMutex = Mutex()
+    // Cache of the latest payments fetched from the network
+    private var payments: List<Payment> = emptyList()
 
-        suspend fun payWithBalance(balancePayment: BalancePayment) {
+
+    suspend fun getPayments(refresh: Boolean = false): List<Payment> {
+        if (refresh || payments.isEmpty()) {
+            val result = remoteDataSource.getPayments()
+            // Thread-safe write to payments
+            paymentsMutex.withLock {
+                this.payments = result.map { it.asModel() }
+            }
+        }
+
+        return paymentsMutex.withLock { this.payments }
+    }
+
+    suspend fun payWithBalance(balancePayment: BalancePayment) {
             remoteDataSource.payWithBalance(balancePayment.asNetworkModel())
         }
 
